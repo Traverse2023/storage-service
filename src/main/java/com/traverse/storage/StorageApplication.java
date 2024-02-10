7706d723fab7ae2738a3bab2a3ceb4b31a29b37e
Isfar Oshir <isfaroshir@gmail.com>
2024-02-09 21:55:20 -0500

    update

--- src/main/java/com/traverse/storage/StorageApplication.java
+++ src/main/java/com/traverse/storage/StorageApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StorageApplication {
-
     public static void main(final String[] args) {
         SpringApplication.run(StorageApplication.class, args);
     }
