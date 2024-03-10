1dd92c6ce9076f9e24dd7e24f8a1b2442120757b
Bryan Palomo <briplomo@gmail.com>
2024-03-10 18:43:21 -0500

    finish dynamo config

--- src/main/java/com/traverse/storage/utils/config/DynamoDBConfig.java
+++ src/main/java/com/traverse/storage/utils/config/DynamoDBConfig.java
@@ -1,18 +1,8 @@
 package com.traverse.storage.utils.config;
 
-<<<<<<< HEAD
-import com.amazonaws.auth.*;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
-=======
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
->>>>>>> 6a080f995d200764ff6136811d47487689e8b03f
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,44 +11,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DynamoDBConfig {
 
-<<<<<<< HEAD
     @Value("${aws.dynamodb.endpoint}")
     private String dynamodbEndpoint;
 
     @Value("${aws.region}")
     private String awsRegion;
-
-    @Value("${aws.dynamodb.secretKey}")
-    private String dynamodbSecretKey;
-
-    @Value("${aws.dynamodb.accessKey}")
-    private String dynamodbAccessKey;
-
-    @Value("${aws.dynamodb.sessionToken}")
-    private String dynamodbSessionToken;
-
-    @Bean
-    public DynamoDBMapper dynamoDBMapper() {
-        return new DynamoDBMapper(buildAmazonDynamoDB());
-    }
-
-    private AmazonDynamoDB buildAmazonDynamoDB() {
-        return AmazonDynamoDBAsyncClientBuilder
-=======
-//    @Value("${aws.dynamodb.endpoint}")
-//    private String dynamodbEndpoint;
-
-//    @Value("${aws.region}")
-//    private String awsRegion;
-
- //   @Value("${aws.dynamodb.accessKey}")
- //   private String dynamodbAccessKey;
-
-  //  @Value("${aws.dynamodb.secretKey}")
- //   private String dynamodbSecretKey;
-
-//    @Value("${aws.dynamo.sessionToken}")
-//    private String dynamodbSessionToken;
 
     @Bean
     public DynamoDBMapper dynamoDBMapper() {
@@ -67,35 +24,15 @@ public class DynamoDBConfig {
 
     public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClientBuilder
->>>>>>> 6a080f995d200764ff6136811d47487689e8b03f
                 .standard()
                 .withEndpointConfiguration(
                         new AwsClientBuilder.EndpointConfiguration(
                                 dynamodbEndpoint,
                                 awsRegion
                         )
-<<<<<<< HEAD
-                )
-                .withCredentials(amazonDynamoDBCredentials())
-                .build();
-    }
 
-    private AWSCredentialsProvider amazonDynamoDBCredentials() {
-        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(dynamodbAccessKey, dynamodbSecretKey));
-    }
-=======
                  )
                 .build();
     }
-
-    //private AWSCredentialsProvider amazonCredentialProvider() {
-   //     return new AWSStaticCredentialsProvider(
-     //           new BasicAWSCredentials(
-      //                  dynamodbAccessKey,
-      //                  dynamodbSecretKey
-        //        )
-     //   );
-    //}
->>>>>>> 6a080f995d200764ff6136811d47487689e8b03f
 
 }
